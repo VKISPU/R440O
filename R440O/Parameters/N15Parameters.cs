54c3ee5f3afcf9c56260e42fa0d64aa6bd16e8d3
MaximKrylov <maxim.kryloff.git@gmail.com>
2014-10-21 11:20:11 +0400

    Добавил класс для определения параметров Н15

---
+++ R440O/Parameters/N15Parameters.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R440O.Parameters
+{
+    class N15Parameters
+    {
+        // Кнопки
+        public static string Н15КнопкаСтанцияВкл { get; set; }
+        public static string Н15КнопкаСтанцияВыкл { get; set; }
+        public static string Н15КнопкаПРМНаведениеЦ300М1 { get; set; }
+        public static string Н15КнопкаПРМНаведениеЦ300М2 { get; set; }
+        public static string Н15КнопкаПРМНаведениеЦ300М3 { get; set; }
+        public static string Н15КнопкаПРМНаведениеЦ300М4 { get; set; }
+        public static string Н15КнопкаМощностьН16 { get; set; }
+        public static string Н15КнопкаМощностьАнт { get; set; }
+        public static string Н15КнопкаМощностьСброс { get; set; }
+        public static string Н15КнопкаН13_1 { get; set; }
+        public static string Н15КнопкаН13_2 { get; set; }
+        public static string Н15КнопкаН13_12 { get; set; }
+        public static string Н15КнопкаСброс { get; set; }
+
+        // Тумблеры
+        public static string Н15ТумблерЦ300М1 { get; set; }
+        public static string Н15ТумблерЦ300М2 { get; set; }
+        public static string Н15ТумблерЦ300М3 { get; set; }
+        public static string Н15ТумблерЦ300М4 { get; set; }
+        public static string Н15ТумблерН12С { get; set; }
+        public static string Н15ТумблерМШУ { get; set; }
+        public static string Н15ТумблерБМА_1 { get; set; }
+        public static string Н15ТумблерБМА_2 { get; set; }
+        public static string Н15ТумблерА205 { get; set; }
+        public static string Н15ТумблерА20512 { get; set; }
+        public static string Н15ТумблерА30412 { get; set; }
+        public static string Н15ТумблерАФСС { get; set; }
+        public static string Н15ТумблерА1 { get; set; }
+        public static string Н15ТумблерА403 { get; set; }
+        public static string Н15ТумблерК1_1 { get; set; }
+        public static string Н15ТумблерК1_2 { get; set; }
+        public static string Н15ТумблерБ1_1 { get; set; }
+        public static string Н15ТумблерБ1_2 { get; set; }
+        public static string Н15ТумблерБ2_1 { get; set; }
+        public static string Н15ТумблерБ2_2 { get; set; }
+        public static string Н15ТумблерБ3_1 { get; set; }
+        public static string Н15ТумблерБ3_2 { get; set; }
+        public static string Н15ТумблерДАБ_5 { get; set; }
+        public static string Н15ТумблерР_Н { get; set; }
+        public static string Н15ТумблерА503Б { get; set; }
+        public static string Н15ТумблерФаза { get; set; }
+        public static string Н15ТумблерУров1 { get; set; }
+        public static string Н15ТумблерУров2 { get; set; }
+        public static string Н15ТумблерАнтЭкв { get; set; }
+        public static string Н15ТумблерТлфТлгПрм { get; set; }
+        public static string Н15ТумблерТлфТлгПрд { get; set; }
+        public static string Н15Тумблер5мГц25мГц3 { get; set; }
+
+        // Лампочки
+        public static string Н15ЛампочкаЦ300МВкл1 { get; set; }
+        public static string Н15ЛампочкаЦ300МВкл2 { get; set; }
+        public static string Н15ЛампочкаЦ300МВкл3 { get; set; }
+        public static string Н15ЛампочкаЦ300МВкл4 { get; set; }
+        public static string Н15ЛампочкаЦ300МСигнал1 { get; set; }
+        public static string Н15ЛампочкаЦ300МСигнал2 { get; set; }
+        public static string Н15ЛампочкаЦ300МСигнал3 { get; set; }
+        public static string Н15ЛампочкаЦ300МСигнал4 { get; set; }
+        public static string Н15ЛампочкаЦ300МНеиспр1 { get; set; }
+        public static string Н15ЛампочкаЦ300МНеиспр2 { get; set; }
+        public static string Н15ЛампочкаЦ300МНеиспр3 { get; set; }
+        public static string Н15ЛампочкаЦ300МНеиспр4 { get; set; }
+        public static string Н15ЛампочкаППВВкл1 { get; set; }
+        public static string Н15ЛампочкаППВВкл2 { get; set; }
+        public static string Н15ЛампочкаППВРабота1 { get; set; }
+        public static string Н15ЛампочкаППВРабота2 { get; set; }
+        public static string Н15ЛампочкаА205Неиспр1 { get; set; }
+        public static string Н15ЛампочкаА205Неиспр2 { get; set; }
+        public static string Н15ЛампочкаУМ1Работа1 { get; set; }
+        public static string Н15ЛампочкаУМ1Работа2 { get; set; }
+        public static string Н15ЛампочкаН12С { get; set; }
+        public static string Н15ЛампочкаМШУ { get; set; }
+        public static string Н15ЛампочкаБМА_1 { get; set; }
+        public static string Н15ЛампочкаБМА_2 { get; set; }
+        public static string Н15Лампочка27В { get; set; }
+        public static string Н15ЛампочкаН15БП { get; set; }
+        public static string Н15ЛампочкаАФСС { get; set; }
+        public static string Н15ЛампочкаА1 { get; set; }
+        public static string Н15ЛампочкаА403Вкл { get; set; }
+        public static string Н15ЛампочкаА403Неиспр { get; set; }
+        public static string Н15ЛампочкаП220272 { get; set; }
+        public static string Н15ЛампочкаП220273 { get; set; }
+        public static string Н15ЛампочкаА306 { get; set; }
+        public static string Н15ЛампочкаА3041 { get; set; }
+        public static string Н15ЛампочкаА3042 { get; set; }
+        public static string Н15ЛампочкаБ1_1 { get; set; }
+        public static string Н15ЛампочкаБ1_2 { get; set; }
+        public static string Н15ЛампочкаБ2_1 { get; set; }
+        public static string Н15ЛампочкаБ2_2 { get; set; }
+        public static string Н15ЛампочкаБ3_1 { get; set; }
+        public static string Н15ЛампочкаБ3_2 { get; set; }
+        public static string Н15ЛампочкаДАБ_5 { get; set; }
+        public static string Н15ЛампочкаР_Н { get; set; }
+        public static string Н15ЛампочкаН16Н13_1 { get; set; }
+        public static string Н15ЛампочкаН16Н13_2 { get; set; }
+        public static string Н15ЛампочкаН16Н13_12 { get; set; }
+        public static string Н15ЛампочкаН13_11Ступень { get; set; }
+        public static string Н15ЛампочкаН13_21Ступень { get; set; }
+        public static string Н15ЛампочкаН13_1ПолноеВкл { get; set; }
+        public static string Н15ЛампочкаН13_2ПолноеВкл { get; set; }
+        public static string Н15ЛампочкаН13_1Неисправность { get; set; }
+        public static string Н15ЛампочкаН13_2Неисправность { get; set; }
+        public static string Н15Лампочка5мГц2 { get; set; }
+        public static string Н15Лампочка5мГц3 { get; set; }
+        public static string Н15ЛампочкаА503Б { get; set; }
+        public static string Н15ЛампочкаАнт { get; set; }
+        public static string Н15ЛампочкаЭкв { get; set; }
+        
+        
+    }
+}
