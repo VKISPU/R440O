07109489d31825330f5be47d22a79f7e6cfa03f9
MaximKrylov <maxim.kryloff.git@gmail.com>
2014-10-28 12:37:56 +0400

    Блок Н15. Добавление кнопок Добавлены анимации кнопок и присвоения значений соответствующим параметрам

--- R440O/R440OForms/N15/N15Form.Designer.cs
+++ R440O/R440OForms/N15/N15Form.Designer.cs
@@ -31,6 +31,17 @@
             this.N15Panel = new System.Windows.Forms.Panel();
             this.Н15КнопкаСтанцияВыкл = new System.Windows.Forms.Button();
             this.Н15КнопкаСтанцияВкл = new System.Windows.Forms.Button();
+            this.Н15КнопкаПРМНаведениеЦ300М1 = new System.Windows.Forms.Button();
+            this.Н15КнопкаПРМНаведениеЦ300М2 = new System.Windows.Forms.Button();
+            this.Н15КнопкаПРМНаведениеЦ300М3 = new System.Windows.Forms.Button();
+            this.Н15КнопкаПРМНаведениеЦ300М4 = new System.Windows.Forms.Button();
+            this.Н15КнопкаМощностьН16 = new System.Windows.Forms.Button();
+            this.Н15КнопкаМощностьАнт = new System.Windows.Forms.Button();
+            this.Н15КнопкаМощностьСброс = new System.Windows.Forms.Button();
+            this.Н15КнопкаН13_1 = new System.Windows.Forms.Button();
+            this.Н15КнопкаН13_2 = new System.Windows.Forms.Button();
+            this.Н15КнопкаН13_12 = new System.Windows.Forms.Button();
+            this.Н15КнопкаСброс = new System.Windows.Forms.Button();
             this.N15Panel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,6 +49,17 @@
             // 
             this.N15Panel.BackgroundImage = global::R440O.BackgroundImages.N15;
             this.N15Panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.N15Panel.Controls.Add(this.Н15КнопкаСброс);
+            this.N15Panel.Controls.Add(this.Н15КнопкаН13_12);
+            this.N15Panel.Controls.Add(this.Н15КнопкаН13_2);
+            this.N15Panel.Controls.Add(this.Н15КнопкаН13_1);
+            this.N15Panel.Controls.Add(this.Н15КнопкаМощностьСброс);
+            this.N15Panel.Controls.Add(this.Н15КнопкаМощностьАнт);
+            this.N15Panel.Controls.Add(this.Н15КнопкаМощностьН16);
+            this.N15Panel.Controls.Add(this.Н15КнопкаПРМНаведениеЦ300М4);
+            this.N15Panel.Controls.Add(this.Н15КнопкаПРМНаведениеЦ300М3);
+            this.N15Panel.Controls.Add(this.Н15КнопкаПРМНаведениеЦ300М2);
+            this.N15Panel.Controls.Add(this.Н15КнопкаПРМНаведениеЦ300М1);
             this.N15Panel.Controls.Add(this.Н15КнопкаСтанцияВыкл);
             this.N15Panel.Controls.Add(this.Н15КнопкаСтанцияВкл);
             this.N15Panel.Location = new System.Drawing.Point(0, 0);
@@ -79,6 +101,201 @@
             this.Н15КнопкаСтанцияВкл.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаСтанцияВкл_MouseDown);
             this.Н15КнопкаСтанцияВкл.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаСтанцияВкл_MouseUp);
             // 
+            // Н15КнопкаПРМНаведениеЦ300М1
+            // 
+            this.Н15КнопкаПРМНаведениеЦ300М1.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М1.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаПРМНаведениеЦ300М1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаПРМНаведениеЦ300М1.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаПРМНаведениеЦ300М1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаПРМНаведениеЦ300М1.Location = new System.Drawing.Point(56, 181);
+            this.Н15КнопкаПРМНаведениеЦ300М1.Name = "Н15КнопкаПРМНаведениеЦ300М1";
+            this.Н15КнопкаПРМНаведениеЦ300М1.Size = new System.Drawing.Size(37, 37);
+            this.Н15КнопкаПРМНаведениеЦ300М1.TabIndex = 2;
+            this.Н15КнопкаПРМНаведениеЦ300М1.Text = "1";
+            this.Н15КнопкаПРМНаведениеЦ300М1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Н15КнопкаПРМНаведениеЦ300М1.UseVisualStyleBackColor = false;
+            this.Н15КнопкаПРМНаведениеЦ300М1.Click += new System.EventHandler(this.Н15КнопкаПРМНаведениеЦ300М1_Click);
+            // 
+            // Н15КнопкаПРМНаведениеЦ300М2
+            // 
+            this.Н15КнопкаПРМНаведениеЦ300М2.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М2.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаПРМНаведениеЦ300М2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаПРМНаведениеЦ300М2.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаПРМНаведениеЦ300М2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаПРМНаведениеЦ300М2.Location = new System.Drawing.Point(91, 181);
+            this.Н15КнопкаПРМНаведениеЦ300М2.Name = "Н15КнопкаПРМНаведениеЦ300М2";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Size = new System.Drawing.Size(36, 37);
+            this.Н15КнопкаПРМНаведениеЦ300М2.TabIndex = 3;
+            this.Н15КнопкаПРМНаведениеЦ300М2.Text = "2";
+            this.Н15КнопкаПРМНаведениеЦ300М2.UseVisualStyleBackColor = false;
+            this.Н15КнопкаПРМНаведениеЦ300М2.Click += new System.EventHandler(this.Н15КнопкаПРМНаведениеЦ300М2_Click);
+            // 
+            // Н15КнопкаПРМНаведениеЦ300М3
+            // 
+            this.Н15КнопкаПРМНаведениеЦ300М3.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М3.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаПРМНаведениеЦ300М3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаПРМНаведениеЦ300М3.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаПРМНаведениеЦ300М3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаПРМНаведениеЦ300М3.Location = new System.Drawing.Point(126, 181);
+            this.Н15КнопкаПРМНаведениеЦ300М3.Name = "Н15КнопкаПРМНаведениеЦ300М3";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Size = new System.Drawing.Size(37, 37);
+            this.Н15КнопкаПРМНаведениеЦ300М3.TabIndex = 4;
+            this.Н15КнопкаПРМНаведениеЦ300М3.Text = "3";
+            this.Н15КнопкаПРМНаведениеЦ300М3.UseVisualStyleBackColor = false;
+            this.Н15КнопкаПРМНаведениеЦ300М3.Click += new System.EventHandler(this.Н15КнопкаПРМНаведениеЦ300М3_Click);
+            // 
+            // Н15КнопкаПРМНаведениеЦ300М4
+            // 
+            this.Н15КнопкаПРМНаведениеЦ300М4.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М4.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаПРМНаведениеЦ300М4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаПРМНаведениеЦ300М4.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаПРМНаведениеЦ300М4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаПРМНаведениеЦ300М4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаПРМНаведениеЦ300М4.Location = new System.Drawing.Point(161, 181);
+            this.Н15КнопкаПРМНаведениеЦ300М4.Name = "Н15КнопкаПРМНаведениеЦ300М4";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Size = new System.Drawing.Size(33, 37);
+            this.Н15КнопкаПРМНаведениеЦ300М4.TabIndex = 5;
+            this.Н15КнопкаПРМНаведениеЦ300М4.Text = "4";
+            this.Н15КнопкаПРМНаведениеЦ300М4.UseVisualStyleBackColor = false;
+            this.Н15КнопкаПРМНаведениеЦ300М4.Click += new System.EventHandler(this.Н15КнопкаПРМНаведениеЦ300М4_Click);
+            // 
+            // Н15КнопкаМощностьН16
+            // 
+            this.Н15КнопкаМощностьН16.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьН16.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаМощностьН16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаМощностьН16.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаМощностьН16.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьН16.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьН16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаМощностьН16.Location = new System.Drawing.Point(195, 181);
+            this.Н15КнопкаМощностьН16.Name = "Н15КнопкаМощностьН16";
+            this.Н15КнопкаМощностьН16.Size = new System.Drawing.Size(35, 37);
+            this.Н15КнопкаМощностьН16.TabIndex = 6;
+            this.Н15КнопкаМощностьН16.Text = "H16";
+            this.Н15КнопкаМощностьН16.UseVisualStyleBackColor = false;
+            this.Н15КнопкаМощностьН16.Click += new System.EventHandler(this.Н15КнопкаМощностьН16_Click);
+            // 
+            // Н15КнопкаМощностьАнт
+            // 
+            this.Н15КнопкаМощностьАнт.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьАнт.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаМощностьАнт.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаМощностьАнт.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаМощностьАнт.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьАнт.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьАнт.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаМощностьАнт.Location = new System.Drawing.Point(230, 181);
+            this.Н15КнопкаМощностьАнт.Name = "Н15КнопкаМощностьАнт";
+            this.Н15КнопкаМощностьАнт.Size = new System.Drawing.Size(38, 37);
+            this.Н15КнопкаМощностьАнт.TabIndex = 7;
+            this.Н15КнопкаМощностьАнт.Text = "АНТ";
+            this.Н15КнопкаМощностьАнт.UseVisualStyleBackColor = false;
+            this.Н15КнопкаМощностьАнт.Click += new System.EventHandler(this.Н15КнопкаМощностьАнт_Click);
+            // 
+            // Н15КнопкаМощностьСброс
+            // 
+            this.Н15КнопкаМощностьСброс.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьСброс.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаМощностьСброс.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаМощностьСброс.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаМощностьСброс.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьСброс.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаМощностьСброс.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаМощностьСброс.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F);
+            this.Н15КнопкаМощностьСброс.Location = new System.Drawing.Point(265, 181);
+            this.Н15КнопкаМощностьСброс.Name = "Н15КнопкаМощностьСброс";
+            this.Н15КнопкаМощностьСброс.Size = new System.Drawing.Size(38, 37);
+            this.Н15КнопкаМощностьСброс.TabIndex = 8;
+            this.Н15КнопкаМощностьСброс.Text = "СБРОС";
+            this.Н15КнопкаМощностьСброс.UseVisualStyleBackColor = false;
+            this.Н15КнопкаМощностьСброс.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаМощностьСброс_MouseDown);
+            this.Н15КнопкаМощностьСброс.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаМощностьСброс_MouseUp);
+            // 
+            // Н15КнопкаН13_1
+            // 
+            this.Н15КнопкаН13_1.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_1.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаН13_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаН13_1.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаН13_1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаН13_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            this.Н15КнопкаН13_1.Location = new System.Drawing.Point(551, 265);
+            this.Н15КнопкаН13_1.Name = "Н15КнопкаН13_1";
+            this.Н15КнопкаН13_1.Size = new System.Drawing.Size(37, 37);
+            this.Н15КнопкаН13_1.TabIndex = 9;
+            this.Н15КнопкаН13_1.Text = "H13-1";
+            this.Н15КнопкаН13_1.UseVisualStyleBackColor = false;
+            this.Н15КнопкаН13_1.Click += new System.EventHandler(this.Н15КнопкаН13_1_Click);
+            // 
+            // Н15КнопкаН13_2
+            // 
+            this.Н15КнопкаН13_2.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_2.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаН13_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаН13_2.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаН13_2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаН13_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            this.Н15КнопкаН13_2.Location = new System.Drawing.Point(586, 265);
+            this.Н15КнопкаН13_2.Name = "Н15КнопкаН13_2";
+            this.Н15КнопкаН13_2.Size = new System.Drawing.Size(36, 37);
+            this.Н15КнопкаН13_2.TabIndex = 10;
+            this.Н15КнопкаН13_2.Text = "Н13-2";
+            this.Н15КнопкаН13_2.UseVisualStyleBackColor = false;
+            this.Н15КнопкаН13_2.Click += new System.EventHandler(this.Н15КнопкаН13_2_Click);
+            // 
+            // Н15КнопкаН13_12
+            // 
+            this.Н15КнопкаН13_12.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_12.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаН13_12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаН13_12.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаН13_12.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаН13_12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаН13_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F);
+            this.Н15КнопкаН13_12.Location = new System.Drawing.Point(621, 265);
+            this.Н15КнопкаН13_12.Name = "Н15КнопкаН13_12";
+            this.Н15КнопкаН13_12.Size = new System.Drawing.Size(37, 37);
+            this.Н15КнопкаН13_12.TabIndex = 11;
+            this.Н15КнопкаН13_12.Text = "Н13-1,2";
+            this.Н15КнопкаН13_12.UseVisualStyleBackColor = false;
+            this.Н15КнопкаН13_12.Click += new System.EventHandler(this.Н15КнопкаН13_12_Click);
+            // 
+            // Н15КнопкаСброс
+            // 
+            this.Н15КнопкаСброс.BackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаСброс.BackgroundImage = global::R440O.ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаСброс.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15КнопкаСброс.FlatAppearance.BorderSize = 0;
+            this.Н15КнопкаСброс.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаСброс.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Н15КнопкаСброс.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Н15КнопкаСброс.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.2F);
+            this.Н15КнопкаСброс.Location = new System.Drawing.Point(656, 265);
+            this.Н15КнопкаСброс.Name = "Н15КнопкаСброс";
+            this.Н15КнопкаСброс.Size = new System.Drawing.Size(37, 37);
+            this.Н15КнопкаСброс.TabIndex = 12;
+            this.Н15КнопкаСброс.Text = "СБРОС";
+            this.Н15КнопкаСброс.UseVisualStyleBackColor = false;
+            this.Н15КнопкаСброс.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаСброс_MouseDown);
+            this.Н15КнопкаСброс.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Н15КнопкаСброс_MouseUp);
+            // 
             // N15Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -101,5 +318,16 @@
         private System.Windows.Forms.Panel N15Panel;
         private System.Windows.Forms.Button Н15КнопкаСтанцияВкл;
         private System.Windows.Forms.Button Н15КнопкаСтанцияВыкл;
+        private System.Windows.Forms.Button Н15КнопкаПРМНаведениеЦ300М1;
+        private System.Windows.Forms.Button Н15КнопкаПРМНаведениеЦ300М2;
+        private System.Windows.Forms.Button Н15КнопкаПРМНаведениеЦ300М3;
+        private System.Windows.Forms.Button Н15КнопкаПРМНаведениеЦ300М4;
+        private System.Windows.Forms.Button Н15КнопкаМощностьН16;
+        private System.Windows.Forms.Button Н15КнопкаМощностьАнт;
+        private System.Windows.Forms.Button Н15КнопкаМощностьСброс;
+        private System.Windows.Forms.Button Н15КнопкаН13_1;
+        private System.Windows.Forms.Button Н15КнопкаН13_2;
+        private System.Windows.Forms.Button Н15КнопкаН13_12;
+        private System.Windows.Forms.Button Н15КнопкаСброс;
     }
 }
--- R440O/R440OForms/N15/N15Form.cs
+++ R440O/R440OForms/N15/N15Form.cs
@@ -45,8 +45,198 @@ namespace R440O.R440OForms.N15
             N15Parameters.Н15КнопкаСтанцияВыкл = "true";
         }
 
+        private void Н15КнопкаПРМНаведениеЦ300М1_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = false;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаПРМНаведениеЦ300М2_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = false;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаПРМНаведениеЦ300М3_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = false;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаПРМНаведениеЦ300М4_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = false;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаМощностьН16_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаМощностьН16.Visible = false;
+            N15Parameters.Н15КнопкаМощностьН16 = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаМощностьАнт_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаМощностьАнт.Visible = false;
+            N15Parameters.Н15КнопкаМощностьАнт = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            N15Parameters.Н15КнопкаМощностьСброс = "false";
+        }
+
+        private void Н15КнопкаМощностьСброс_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Н15КнопкаМощностьСброс.BackgroundImage = ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаМощностьСброс.Text = "СБРОС";
+            N15Parameters.Н15КнопкаМощностьСброс = "true";
+
+            this.Н15КнопкаПРМНаведениеЦ300М1.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М1 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М2.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М2 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М3.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М3 = "false";
+            this.Н15КнопкаПРМНаведениеЦ300М4.Visible = true;
+            N15Parameters.Н15КнопкаПРМНаведениеЦ300М4 = "false";
+            this.Н15КнопкаМощностьН16.Visible = true;
+            N15Parameters.Н15КнопкаМощностьН16 = "false";
+            this.Н15КнопкаМощностьАнт.Visible = true;
+            N15Parameters.Н15КнопкаМощностьАнт = "false";
+        }
+
+        private void Н15КнопкаМощностьСброс_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Н15КнопкаМощностьСброс.BackgroundImage = null;
+            this.Н15КнопкаМощностьСброс.Text = "";
+        }
+
         private void N15Form_Load(object sender, EventArgs e)
         {
+        }
+
+        private void Н15КнопкаН13_1_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаН13_1.Visible = false;
+            N15Parameters.Н15КнопкаН13_1 = "true";
+
+            this.Н15КнопкаН13_2.Visible = true;
+            N15Parameters.Н15КнопкаН13_2 = "false";
+            this.Н15КнопкаН13_12.Visible = true;
+            N15Parameters.Н15КнопкаН13_12 = "false";
+            N15Parameters.Н15КнопкаСброс = "false";
+        }
+
+        private void Н15КнопкаН13_2_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаН13_2.Visible = false;
+            N15Parameters.Н15КнопкаН13_2 = "true";
+
+            this.Н15КнопкаН13_1.Visible = true;
+            N15Parameters.Н15КнопкаН13_1 = "false";
+            this.Н15КнопкаН13_12.Visible = true;
+            N15Parameters.Н15КнопкаН13_12 = "false";
+            N15Parameters.Н15КнопкаСброс = "false";
+        }
+
+        private void Н15КнопкаН13_12_Click(object sender, EventArgs e)
+        {
+            this.Н15КнопкаН13_12.Visible = false;
+            N15Parameters.Н15КнопкаН13_12 = "true";
+
+            this.Н15КнопкаН13_1.Visible = true;
+            N15Parameters.Н15КнопкаН13_1 = "false";
+            this.Н15КнопкаН13_2.Visible = true;
+            N15Parameters.Н15КнопкаН13_2 = "false";
+            N15Parameters.Н15КнопкаСброс = "false";
+        }
+
+        private void Н15КнопкаСброс_MouseUp(object sender, MouseEventArgs e)
+        {            
+            this.Н15КнопкаСброс.BackgroundImage = ControlElementImages.buttonSquareType1UpWhite;
+            this.Н15КнопкаСброс.Text = "СБРОС";
+            N15Parameters.Н15КнопкаСброс = "true";
+
+            this.Н15КнопкаН13_1.Visible = true;
+            N15Parameters.Н15КнопкаН13_1 = "false";
+            this.Н15КнопкаН13_2.Visible = true;
+            N15Parameters.Н15КнопкаН13_2 = "false";
+            this.Н15КнопкаН13_12.Visible = true;
+            N15Parameters.Н15КнопкаН13_12 = "false";
+        }
+
+        private void Н15КнопкаСброс_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Н15КнопкаСброс.BackgroundImage = null;
+            this.Н15КнопкаСброс.Text = "";
         }
     }
 }
