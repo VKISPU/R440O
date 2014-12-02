d4edc826db2e53aad6d1141e20b38d4effb2cd4a
MaximKrylov <maxim.kryloff.git@gmail.com>
2014-12-02 14:30:34 +0400

    Создал параметры для блока Ц300М_1

---
+++ R440O/Parameters/C300M_1Parameters.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R440O.Parameters
+{
+    class C300M_1Parameters
+    {
+        #region Кнопки ВИД РАБОТЫ
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы0d025 { get { return _Ц300М_1КнопкаВидРаботы0d025; } set { _Ц300М_1КнопкаВидРаботы0d025 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы0d025 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы0d05 { get { return _Ц300М_1КнопкаВидРаботы0d05; } set { _Ц300М_1КнопкаВидРаботы0d05 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы0d05 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы0d1 { get { return _Ц300М_1КнопкаВидРаботы0d1; } set { _Ц300М_1КнопкаВидРаботы0d1 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы0d1 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы1d2 { get { return _Ц300М_1КнопкаВидРаботы1d2; } set { _Ц300М_1КнопкаВидРаботы1d2 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы1d2 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы2d4 { get { return _Ц300М_1КнопкаВидРаботы2d4; } set { _Ц300М_1КнопкаВидРаботы2d4 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы2d4 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы4d8 { get { return _Ц300М_1КнопкаВидРаботы4d8; } set { _Ц300М_1КнопкаВидРаботы4d8 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы4d8 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы48 { get { return _Ц300М_1КнопкаВидРаботы48; } set { _Ц300М_1КнопкаВидРаботы48 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы48 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы96144 { get { return _Ц300М_1КнопкаВидРаботы96144; } set { _Ц300М_1КнопкаВидРаботы96144 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы96144 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы240 { get { return _Ц300М_1КнопкаВидРаботы240; } set { _Ц300М_1КнопкаВидРаботы240 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы240 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботы480 { get { return _Ц300М_1КнопкаВидРаботы480; } set { _Ц300М_1КнопкаВидРаботы480 = value; } }
+        private static string _Ц300М_1КнопкаВидРаботы480 = "false";
+
+        /// <summary>
+        /// Возможные состояния: true, false
+        /// </summary>
+        public static string Ц300М_1КнопкаВидРаботыСброс { get { return _Ц300М_1КнопкаВидРаботыСброс; } set { _Ц300М_1КнопкаВидРаботыСброс = value; } }
+        private static string _Ц300М_1КнопкаВидРаботыСброс = "false";
+        #endregion
+    }
+}
