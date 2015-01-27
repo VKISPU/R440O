55e6aac750f7286501a615052548f403c9ffd57a
MaximKrylov <maxim.kryloff.git@gmail.com>
2015-01-27 15:17:13 +0300

    Контур-П3. Сделал часть параметров

--- R440O/R440OForms/Kontur_P3/Kontur_P3Form.Designer.cs
+++ R440O/R440OForms/Kontur_P3/Kontur_P3Form.Designer.cs
@@ -29,16 +29,30 @@
         private void InitializeComponent()
         {
             this.Kontur_P3Panel = new System.Windows.Forms.Panel();
+            this.Н15ЛампочкаЦ300МВкл4 = new System.Windows.Forms.PictureBox();
+            this.Kontur_P3Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Н15ЛампочкаЦ300МВкл4)).BeginInit();
             this.SuspendLayout();
             // 
             // Kontur_P3Panel
             // 
             this.Kontur_P3Panel.BackgroundImage = global::R440O.BackgroundImages.Kontur_P3;
             this.Kontur_P3Panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Kontur_P3Panel.Controls.Add(this.Н15ЛампочкаЦ300МВкл4);
             this.Kontur_P3Panel.Location = new System.Drawing.Point(0, 0);
             this.Kontur_P3Panel.Name = "Kontur_P3Panel";
             this.Kontur_P3Panel.Size = new System.Drawing.Size(1109, 409);
             this.Kontur_P3Panel.TabIndex = 1;
+            // 
+            // Н15ЛампочкаЦ300МВкл4
+            // 
+            this.Н15ЛампочкаЦ300МВкл4.BackColor = System.Drawing.Color.Transparent;
+            this.Н15ЛампочкаЦ300МВкл4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Н15ЛампочкаЦ300МВкл4.Location = new System.Drawing.Point(112, 66);
+            this.Н15ЛампочкаЦ300МВкл4.Name = "Н15ЛампочкаЦ300МВкл4";
+            this.Н15ЛампочкаЦ300МВкл4.Size = new System.Drawing.Size(17, 19);
+            this.Н15ЛампочкаЦ300МВкл4.TabIndex = 49;
+            this.Н15ЛампочкаЦ300МВкл4.TabStop = false;
             // 
             // Kontur_P3Form
             // 
@@ -51,6 +65,8 @@
             this.Name = "Kontur_P3Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Контур-П";
+            this.Kontur_P3Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Н15ЛампочкаЦ300МВкл4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +74,6 @@
         #endregion
 
         private System.Windows.Forms.Panel Kontur_P3Panel;
+        private System.Windows.Forms.PictureBox Н15ЛампочкаЦ300МВкл4;
     }
 }
