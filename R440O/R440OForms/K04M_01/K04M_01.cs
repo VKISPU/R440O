f9b1f1907b4549a68d0924f0192d8ffad66902c5
usehotkey <igolopolosov@gmail.com>
2015-01-27 15:20:39 +0300

    #27 Кулон. Управляющие элементы блока К03М

--- R440O/R440OForms/K04M_01/K04M_01.cs
+++ R440O/R440OForms/K04M_01/K04M_01.cs
@@ -4,6 +4,10 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System.Linq;
+using R440O.Parameters;
+using R440O.ThirdParty;
+
 namespace R440O.R440OForms.K04M_01
 {
     using System.Windows.Forms;
@@ -19,16 +23,48 @@ namespace R440O.R440OForms.K04M_01
         public K04M_01Form()
         {
             this.InitializeComponent();
+            this.InitializeToggles();
         }
 
-        /// <summary>
-        /// Снятие крышки на форме блока
-        /// </summary>
-        /// <param name="sender">Объет вызвавший событие</param>
-        /// <param name="e">Событие закрытия формы</param>
-        private void K04M_01Cover_Click(object sender, System.EventArgs e)
+        private void InitializeToggles()
         {
-            K04M_01Cover.Visible = false;
+            foreach (Control item in K04M_01Panel.Controls)
+            {
+                var fieldList = typeof(K04M_01Parameters).GetProperties();
+                foreach (var field in fieldList.Where(property => item.Name == property.Name))
+                {
+                    if (item.Name.Contains("K04M_01Переключатель"))
+                    {
+                        var angle = (int)field.GetValue(null) * 26 - 120;
+                        item.BackgroundImage =
+                            TransformImageHelper.RotateImageByAngle(ControlElementImages.toggleType2, angle);
+                    }
+                }
+            }
+        }
+
+        private void K04M_01Переключатель_MouseDown(object sender, MouseEventArgs e)
+        {
+            var item = sender as Button;
+            var property = typeof (K04M_01Parameters).GetProperty(item.Name);
+            if (e.Button == MouseButtons.Left)
+            {
+                property.SetValue(null, (int) property.GetValue(null) + 1);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                property.SetValue(null, (int) property.GetValue(null) - 1);
+            }
+
+            var angle = (int) property.GetValue(null)*26 - 120;
+            item.BackgroundImage =
+                TransformImageHelper.RotateImageByAngle(ControlElementImages.toggleType2, angle);
+        }
+
+        private void K04M_01Крышка_Click(object sender, System.EventArgs e)
+        {
+            K04M_01Крышка.Visible = false;
         }
     }
 }
