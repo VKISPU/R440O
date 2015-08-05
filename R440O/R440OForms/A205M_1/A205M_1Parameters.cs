bd3bd10c5040e5d9ed6aaff71a9b985f653e1ac3
MaximKrylov <maxim.kryloff@gmail.com>
2015-08-05 12:30:05 +0400

    Добавление к предыдущему комиту

--- R440O/R440OForms/A205M_1/A205M_1Parameters.cs
+++ R440O/R440OForms/A205M_1/A205M_1Parameters.cs
@@ -2,6 +2,7 @@
 using R440O.R440OForms.N18_M;
 using R440O.R440OForms.N502B;
 using R440O.R440OForms.NKN_1;
+using R440O.R440OForms.C300M_1;
 
 namespace R440O.R440OForms.A205M_1
 {
@@ -199,6 +200,7 @@ namespace R440O.R440OForms.A205M_1
                 if (value > 0 && value < 5)
                 {
                     _переключательВидРаботы = value;
+                    C300M_1Parameters.Search();
                     if (RefreshForm != null) RefreshForm();
                 }
             }
