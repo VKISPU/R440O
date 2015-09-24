8f1e5c49bad39a17db51b8f7453f89d67ba28bc9
nexerd <vitaliy-suhov@mail.ru>
2015-09-24 23:37:51 +0400

    Рефакторинг K05m_1

--- R440O/R440OForms/K05M_01Inside/K05M_01InsideParameters.cs
+++ R440O/R440OForms/K05M_01Inside/K05M_01InsideParameters.cs
@@ -1,9 +1,50 @@
-﻿namespace R440O.Parameters
+﻿namespace R440O.R440OForms.K05M_01Inside
 {
     internal class K05M_01InsideParameters
     {
+#region событие
+        public delegate void ParameterChangedHandler();
+        public static event ParameterChangedHandler ParameterChanged;
+
+        private static void OnParameterChanged()
+        {
+            var handler = ParameterChanged;
+            if (handler != null) handler();
+        }
+
+        public static void ResetParameters()
+        {
+            OnParameterChanged();
+        }
+#endregion
+
         public static KulonIndexerClass Переключатель = new KulonIndexerClass();
         public static bool ТумблерВ4;
         public static bool ТумблерВ7;
+
     }
+    class KulonIndexerClass
+    {
+        private int[] myArray = new int[31];
+        public int this[int index]
+        {
+            get
+            {
+                return myArray[index];
+            }
+            set
+            {
+                if ((index >= 1 && index <= 3) || (index >= 5 && index <= 9))
+                {
+                    if (value >= 0 && value <= 7) myArray[index] = value;
+                }
+                else
+                {
+                    if (value >= 0 && value <= 1) myArray[index] = value;
+                }
+                K05M_01InsideParameters.ResetParameters();
+            }
+        }
+    }
+
 }
