7a063d3e05db684bb1512f3673fd4526d9d92044
REVOLUTiON56 <WeW13@bk.ru>
2015-12-31 19:06:48 +0300

    #Рефакторинг блока Ваттметр

--- R440O/R440OForms/Wattmeter/WattmeterForm.Designer.cs
+++ R440O/R440OForms/Wattmeter/WattmeterForm.Designer.cs
@@ -28,119 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.WattmeterPanel = new System.Windows.Forms.Panel();
-            this.ВаттметрПереключательГрубо = new System.Windows.Forms.Button();
-            this.ВаттметрПереключательРежимРаботы = new System.Windows.Forms.Button();
-            this.ВаттметрПереключательТочно = new System.Windows.Forms.Button();
-            this.ВаттметрПереключательКоррекция = new System.Windows.Forms.Button();
-            this.WattmeterPanel.SuspendLayout();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.Дисплей = new System.Windows.Forms.Button();
+            this.ТумблерСеть = new System.Windows.Forms.Button();
+            this.РегуляторКоррекция = new System.Windows.Forms.Button();
+            this.РегуляторТочно = new System.Windows.Forms.Button();
+            this.РегуляторГрубо = new System.Windows.Forms.Button();
+            this.ПереключательРежимРаботы = new System.Windows.Forms.Button();
+            this.Panel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // WattmeterPanel
+            // Panel
             // 
-            this.WattmeterPanel.BackgroundImage = global::R440O.BackgroundImages.Wattmeter;
-            this.WattmeterPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.WattmeterPanel.Controls.Add(this.ВаттметрПереключательКоррекция);
-            this.WattmeterPanel.Controls.Add(this.ВаттметрПереключательТочно);
-            this.WattmeterPanel.Controls.Add(this.ВаттметрПереключательГрубо);
-            this.WattmeterPanel.Controls.Add(this.ВаттметрПереключательРежимРаботы);
-            this.WattmeterPanel.Location = new System.Drawing.Point(0, 0);
-            this.WattmeterPanel.Name = "WattmeterPanel";
-            this.WattmeterPanel.Size = new System.Drawing.Size(707, 544);
-            this.WattmeterPanel.TabIndex = 0;
+            this.Panel.BackgroundImage = global::R440O.BackgroundImages.Wattmeter;
+            this.Panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Panel.Controls.Add(this.Дисплей);
+            this.Panel.Controls.Add(this.ТумблерСеть);
+            this.Panel.Controls.Add(this.РегуляторКоррекция);
+            this.Panel.Controls.Add(this.РегуляторТочно);
+            this.Panel.Controls.Add(this.РегуляторГрубо);
+            this.Panel.Controls.Add(this.ПереключательРежимРаботы);
+            this.Panel.Location = new System.Drawing.Point(0, 0);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(707, 544);
+            this.Panel.TabIndex = 0;
             // 
-            // ВаттметрПереключательГрубо
+            // Дисплей
             // 
-            this.ВаттметрПереключательГрубо.BackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательГрубо.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
-            this.ВаттметрПереключательГрубо.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ВаттметрПереключательГрубо.FlatAppearance.BorderSize = 0;
-            this.ВаттметрПереключательГрубо.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательГрубо.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательГрубо.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ВаттметрПереключательГрубо.Location = new System.Drawing.Point(214, 295);
-            this.ВаттметрПереключательГрубо.Name = "ВаттметрПереключательГрубо";
-            this.ВаттметрПереключательГрубо.Size = new System.Drawing.Size(76, 71);
-            this.ВаттметрПереключательГрубо.TabIndex = 35;
-            this.ВаттметрПереключательГрубо.UseVisualStyleBackColor = false;
-            this.ВаттметрПереключательГрубо.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательГрубо_MouseDown);
-            this.ВаттметрПереключательГрубо.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательГрубо_MouseMove);
-            this.ВаттметрПереключательГрубо.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательГрубо_MouseUp);
+            this.Дисплей.BackColor = System.Drawing.Color.Transparent;
+            this.Дисплей.FlatAppearance.BorderSize = 0;
+            this.Дисплей.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Дисплей.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Дисплей.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Дисплей.Font = new System.Drawing.Font("Consolas", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Дисплей.ForeColor = System.Drawing.Color.Red;
+            this.Дисплей.Location = new System.Drawing.Point(99, 98);
+            this.Дисплей.Name = "Дисплей";
+            this.Дисплей.Size = new System.Drawing.Size(166, 58);
+            this.Дисплей.TabIndex = 65;
+            this.Дисплей.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Дисплей.UseVisualStyleBackColor = false;
             // 
-            // ВаттметрПереключательРежимРаботы
+            // ТумблерСеть
             // 
-            this.ВаттметрПереключательРежимРаботы.BackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательРежимРаботы.BackgroundImage = global::R440O.ControlElementImages.toggleType3;
-            this.ВаттметрПереключательРежимРаботы.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ВаттметрПереключательРежимРаботы.FlatAppearance.BorderSize = 0;
-            this.ВаттметрПереключательРежимРаботы.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательРежимРаботы.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательРежимРаботы.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ВаттметрПереключательРежимРаботы.Location = new System.Drawing.Point(303, 415);
-            this.ВаттметрПереключательРежимРаботы.Name = "ВаттметрПереключательРежимРаботы";
-            this.ВаттметрПереключательРежимРаботы.Size = new System.Drawing.Size(76, 71);
-            this.ВаттметрПереключательРежимРаботы.TabIndex = 34;
-            this.ВаттметрПереключательРежимРаботы.UseVisualStyleBackColor = false;
-            this.ВаттметрПереключательРежимРаботы.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательРежимРаботы_MouseUp);
+            this.ТумблерСеть.BackColor = System.Drawing.Color.Transparent;
+            this.ТумблерСеть.BackgroundImage = global::R440O.ControlElementImages.tumblerType4Down;
+            this.ТумблерСеть.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ТумблерСеть.FlatAppearance.BorderSize = 0;
+            this.ТумблерСеть.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.ТумблерСеть.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.ТумблерСеть.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ТумблерСеть.Location = new System.Drawing.Point(81, 298);
+            this.ТумблерСеть.Name = "ТумблерСеть";
+            this.ТумблерСеть.Size = new System.Drawing.Size(39, 64);
+            this.ТумблерСеть.TabIndex = 38;
+            this.ТумблерСеть.UseVisualStyleBackColor = false;
+            this.ТумблерСеть.Click += new System.EventHandler(this.ТумблерСеть_Click);
             // 
-            // ВаттметрПереключательТочно
+            // РегуляторКоррекция
             // 
-            this.ВаттметрПереключательТочно.BackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательТочно.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
-            this.ВаттметрПереключательТочно.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ВаттметрПереключательТочно.FlatAppearance.BorderSize = 0;
-            this.ВаттметрПереключательТочно.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательТочно.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательТочно.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ВаттметрПереключательТочно.Location = new System.Drawing.Point(379, 295);
-            this.ВаттметрПереключательТочно.Name = "ВаттметрПереключательТочно";
-            this.ВаттметрПереключательТочно.Size = new System.Drawing.Size(76, 71);
-            this.ВаттметрПереключательТочно.TabIndex = 36;
-            this.ВаттметрПереключательТочно.UseVisualStyleBackColor = false;
-            this.ВаттметрПереключательТочно.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательТочно_MouseDown);
-            this.ВаттметрПереключательТочно.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательТочно_MouseMove);
-            this.ВаттметрПереключательТочно.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательТочно_MouseUp);
+            this.РегуляторКоррекция.BackColor = System.Drawing.Color.Transparent;
+            this.РегуляторКоррекция.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
+            this.РегуляторКоррекция.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.РегуляторКоррекция.FlatAppearance.BorderSize = 0;
+            this.РегуляторКоррекция.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторКоррекция.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторКоррекция.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.РегуляторКоррекция.Location = new System.Drawing.Point(541, 295);
+            this.РегуляторКоррекция.Name = "РегуляторКоррекция";
+            this.РегуляторКоррекция.Size = new System.Drawing.Size(76, 71);
+            this.РегуляторКоррекция.TabIndex = 37;
+            this.РегуляторКоррекция.UseVisualStyleBackColor = false;
+            this.РегуляторКоррекция.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseDown);
+            this.РегуляторКоррекция.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseMove);
+            this.РегуляторКоррекция.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseUp);
             // 
-            // ВаттметрПереключательКоррекция
+            // РегуляторТочно
             // 
-            this.ВаттметрПереключательКоррекция.BackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательКоррекция.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
-            this.ВаттметрПереключательКоррекция.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ВаттметрПереключательКоррекция.FlatAppearance.BorderSize = 0;
-            this.ВаттметрПереключательКоррекция.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательКоррекция.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.ВаттметрПереключательКоррекция.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ВаттметрПереключательКоррекция.Location = new System.Drawing.Point(541, 295);
-            this.ВаттметрПереключательКоррекция.Name = "ВаттметрПереключательКоррекция";
-            this.ВаттметрПереключательКоррекция.Size = new System.Drawing.Size(76, 71);
-            this.ВаттметрПереключательКоррекция.TabIndex = 37;
-            this.ВаттметрПереключательКоррекция.UseVisualStyleBackColor = false;
-            this.ВаттметрПереключательКоррекция.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательКоррекция_MouseDown);
-            this.ВаттметрПереключательКоррекция.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательКоррекция_MouseMove);
-            this.ВаттметрПереключательКоррекция.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ВаттметрПереключательКоррекция_MouseUp);
+            this.РегуляторТочно.BackColor = System.Drawing.Color.Transparent;
+            this.РегуляторТочно.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
+            this.РегуляторТочно.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.РегуляторТочно.FlatAppearance.BorderSize = 0;
+            this.РегуляторТочно.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторТочно.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторТочно.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.РегуляторТочно.Location = new System.Drawing.Point(379, 295);
+            this.РегуляторТочно.Name = "РегуляторТочно";
+            this.РегуляторТочно.Size = new System.Drawing.Size(76, 71);
+            this.РегуляторТочно.TabIndex = 36;
+            this.РегуляторТочно.UseVisualStyleBackColor = false;
+            this.РегуляторТочно.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseDown);
+            this.РегуляторТочно.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseMove);
+            this.РегуляторТочно.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseUp);
+            // 
+            // РегуляторГрубо
+            // 
+            this.РегуляторГрубо.BackColor = System.Drawing.Color.Transparent;
+            this.РегуляторГрубо.BackgroundImage = global::R440O.ControlElementImages.revolverRoundBlack;
+            this.РегуляторГрубо.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.РегуляторГрубо.FlatAppearance.BorderSize = 0;
+            this.РегуляторГрубо.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторГрубо.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.РегуляторГрубо.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.РегуляторГрубо.Location = new System.Drawing.Point(214, 295);
+            this.РегуляторГрубо.Name = "РегуляторГрубо";
+            this.РегуляторГрубо.Size = new System.Drawing.Size(76, 71);
+            this.РегуляторГрубо.TabIndex = 35;
+            this.РегуляторГрубо.UseVisualStyleBackColor = false;
+            this.РегуляторГрубо.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseDown);
+            this.РегуляторГрубо.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseMove);
+            this.РегуляторГрубо.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Регулятор_MouseUp);
+            // 
+            // ПереключательРежимРаботы
+            // 
+            this.ПереключательРежимРаботы.BackColor = System.Drawing.Color.Transparent;
+            this.ПереключательРежимРаботы.BackgroundImage = global::R440O.ControlElementImages.toggleType3;
+            this.ПереключательРежимРаботы.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ПереключательРежимРаботы.FlatAppearance.BorderSize = 0;
+            this.ПереключательРежимРаботы.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.ПереключательРежимРаботы.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.ПереключательРежимРаботы.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ПереключательРежимРаботы.Location = new System.Drawing.Point(303, 415);
+            this.ПереключательРежимРаботы.Name = "ПереключательРежимРаботы";
+            this.ПереключательРежимРаботы.Size = new System.Drawing.Size(76, 71);
+            this.ПереключательРежимРаботы.TabIndex = 34;
+            this.ПереключательРежимРаботы.UseVisualStyleBackColor = false;
+            this.ПереключательРежимРаботы.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ПереключательРежимРаботы_MouseUp);
             // 
             // WattmeterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(707, 542);
-            this.Controls.Add(this.WattmeterPanel);
+            this.Controls.Add(this.Panel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "WattmeterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "БЛОК ВАТТМЕТРА ИЗМЕРИТЕЛЬНЫЙ Я2М-66";
-            this.WattmeterPanel.ResumeLayout(false);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WattmeterForm_FormClosed);
+            this.Panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel WattmeterPanel;
-        private System.Windows.Forms.Button ВаттметрПереключательРежимРаботы;
-        private System.Windows.Forms.Button ВаттметрПереключательГрубо;
-        private System.Windows.Forms.Button ВаттметрПереключательТочно;
-        private System.Windows.Forms.Button ВаттметрПереключательКоррекция;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Button ПереключательРежимРаботы;
+        private System.Windows.Forms.Button РегуляторГрубо;
+        private System.Windows.Forms.Button РегуляторТочно;
+        private System.Windows.Forms.Button РегуляторКоррекция;
+        private System.Windows.Forms.Button ТумблерСеть;
+        private System.Windows.Forms.Button Дисплей;
     }
 }
--- R440O/R440OForms/Wattmeter/WattmeterForm.cs
+++ R440O/R440OForms/Wattmeter/WattmeterForm.cs
@@ -4,17 +4,18 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using R440O.BaseClasses;
+
 namespace R440O.R440OForms.Wattmeter
 {
     using System.Windows.Forms;
-    using Parameters;
     using ThirdParty;
     using System.Reflection;
 
     /// <summary>
     /// Форма блока ватметр
     /// </summary>
-    public partial class WattmeterForm : Form
+    public partial class WattmeterForm : Form, IRefreshableForm
     {
         private static bool isManipulation = false;
 
@@ -23,128 +24,87 @@ namespace R440O.R440OForms.Wattmeter
         /// </summary>
         public WattmeterForm()
         {
-            this.InitializeComponent();
-            InitializeTogglesPosition();
+            InitializeComponent();
+            WattmeterParameters.ParameterChanged += RefreshFormElements;
+            RefreshFormElements();
         }
 
         /// <summary>
         /// Установка переключателей в положение последней их установки
         /// </summary>
-        private void InitializeTogglesPosition()
+        public void RefreshFormElements()
         {
-            foreach (Control item in WattmeterPanel.Controls)
+            var angle = WattmeterParameters.ПереключательРежимРаботы * 30 - 105;
+            ПереключательРежимРаботы.BackgroundImage =
+                TransformImageHelper.RotateImageByAngle(ControlElementImages.toggleType3, angle);
+
+            ТумблерСеть.BackgroundImage = WattmeterParameters.ТумблерСеть
+                ? ControlElementImages.tumblerType4Up
+                : ControlElementImages.tumblerType4Down;
+
+            foreach (Control item in Panel.Controls)
             {
-                if (item.Name.Contains("РежимРаботы"))
+                if (item.Name.Contains("Регулятор"))
                 {
                     PropertyInfo[] fieldList = typeof(WattmeterParameters).GetProperties();
                     foreach (PropertyInfo property in fieldList)
                     {
                         if (item.Name == property.Name)
                         {
-                            var angle = System.Convert.ToInt32(property.GetValue(null)) * 30 - 105;
-                            item.BackgroundImage = TransformImageHelper.RotateImageByAngle(ControlElementImages.toggleType3, angle);
+                            item.BackgroundImage = TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, System.Convert.ToInt32(property.GetValue(null)));
                             break;
                         }
                     }
                 }
-                else
-                    if (item.Name.Contains("Переключатель"))
-                    {
-                        PropertyInfo[] fieldList = typeof(WattmeterParameters).GetProperties();
-                        foreach (PropertyInfo property in fieldList)
-                        {
-                            if (item.Name == property.Name)
-                            {
-                                item.BackgroundImage = TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, System.Convert.ToInt32(property.GetValue(null)));
-                                break;
-                            }
-                        }
-                    }
             }
         }
 
-        private void ВаттметрПереключательРежимРаботы_MouseUp(object sender, MouseEventArgs e)
+        private void Регулятор_MouseUp(object sender, MouseEventArgs e)
+        {
+            isManipulation = false;
+        }
+
+        private void Регулятор_MouseDown(object sender, MouseEventArgs e)
+        {
+            isManipulation = true;
+        }
+
+        private void Регулятор_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isManipulation) return;
+            var button = sender as Button;
+            var angle = TransformImageHelper.CalculateAngle(button.Width, button.Height, e);
+            var property = typeof(WattmeterParameters).GetProperty(button.Name);
+            property.SetValue(null, angle);
+
+            button.BackgroundImage =
+                TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, (int)property.GetValue(null));
+        }
+
+        private void ПереключательРежимРаботы_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                WattmeterParameters.ВаттметрПереключательРежимРаботы += 1;
+                WattmeterParameters.ПереключательРежимРаботы += 1;
             }
 
             if (e.Button == MouseButtons.Right)
             {
-                WattmeterParameters.ВаттметрПереключательРежимРаботы -= 1;
+                WattmeterParameters.ПереключательРежимРаботы -= 1;
             }
-
-            var angle = WattmeterParameters.ВаттметрПереключательРежимРаботы * 30 - 105;
-            ВаттметрПереключательРежимРаботы.BackgroundImage =
-                TransformImageHelper.RotateImageByAngle(ControlElementImages.toggleType3, angle);
         }
 
-        #region ВаттметрПереключательГрубо
-        private void ВаттметрПереключательГрубо_MouseDown(object sender, MouseEventArgs e)
+        private void ТумблерСеть_Click(object sender, System.EventArgs e)
         {
-            isManipulation = true;
+            WattmeterParameters.ТумблерСеть = !WattmeterParameters.ТумблерСеть;
         }
 
-        private void ВаттметрПереключательГрубо_MouseUp(object sender, MouseEventArgs e)
+        private void WattmeterForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            isManipulation = false;
+            WattmeterParameters.ParameterChanged -= RefreshFormElements;
         }
 
-        private void ВаттметрПереключательГрубо_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!isManipulation) return;
-            var button = sender as Button;
-            var angle = TransformImageHelper.CalculateAngle(button.Width, button.Height, e);
-            WattmeterParameters.ВаттметрПереключательГрубо = angle;
-            ВаттметрПереключательГрубо.BackgroundImage =
-                TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, WattmeterParameters.ВаттметрПереключательГрубо);
-        }
-        #endregion
 
-        #region ВаттметрПереключательТочно
-        private void ВаттметрПереключательТочно_MouseUp(object sender, MouseEventArgs e)
-        {
-            isManipulation = false;
-        }
-
-        private void ВаттметрПереключательТочно_MouseDown(object sender, MouseEventArgs e)
-        {
-            isManipulation = true;
-        }
-
-        private void ВаттметрПереключательТочно_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!isManipulation) return;
-            var button = sender as Button;
-            var angle = TransformImageHelper.CalculateAngle(button.Width, button.Height, e);
-            WattmeterParameters.ВаттметрПереключательТочно = angle;
-            ВаттметрПереключательТочно.BackgroundImage =
-                TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, WattmeterParameters.ВаттметрПереключательТочно);
-        }
-        #endregion
-
-        #region ВаттметрПереключательКоррекция
-        private void ВаттметрПереключательКоррекция_MouseDown(object sender, MouseEventArgs e)
-        {
-            isManipulation = true;
-        }
-
-        private void ВаттметрПереключательКоррекция_MouseUp(object sender, MouseEventArgs e)
-        {
-            isManipulation = false;
-        }
-
-        private void ВаттметрПереключательКоррекция_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!isManipulation) return;
-            var button = sender as Button;
-            var angle = TransformImageHelper.CalculateAngle(button.Width, button.Height, e);
-            WattmeterParameters.ВаттметрПереключательКоррекция = angle;
-            ВаттметрПереключательКоррекция.BackgroundImage =
-                TransformImageHelper.RotateImageByAngle(ControlElementImages.revolverRoundBlack, WattmeterParameters.ВаттметрПереключательКоррекция);
-        }
-        #endregion
 
     }
 }
