fdf8d56247127fff3c97ef382a30a140ad9b2da6
Vitaliy Suhov <vitaliy-suhov@bk.com>
2017-01-18 12:26:43 +0300

    server signal response implemented

---
+++ R440O/HttpHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using SignalTypes;
+using System.Threading.Tasks;
+
+namespace R440O
+{
+    class HttpHelper
+    {
+        static async Task<List<Signal>> SendSignal(Signal signal)
+        {
+            string url = "http://localhost:8080/";
+            var body = new StringContent(JsonConvert.SerializeObject(signal), Encoding.UTF8, "application/json");
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.PostAsync(url, body))
+            using (HttpContent content = response.Content)
+            {                
+                string result = await content.ReadAsStringAsync();
+                if (result != null)
+                {
+                    try
+                    {
+                        var signals = JsonConvert.DeserializeObject<List<Signal>>(result);
+                        return signals;
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
--- Retranslator/Server.cs
+++ Retranslator/Server.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using Newtonsoft.Json;
+using SignalTypes;
 
 namespace Retranslator
 {
@@ -26,33 +28,52 @@ namespace Retranslator
                 HttpListenerContext context = httpListener.GetContext();
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
-
-                switch (request.HttpMethod)
+                if (request.HttpMethod == "POST")
+                    Post(request, response);
+                else
                 {
-                    case "GET":
-                        break;
-                    case "POST":
-                        Post(request.RawUrl, response);
-                        break;
+                    Error(response);
                 }
             }
         }
 
-        private void Post(string url, HttpListenerResponse response)
+        private void Error(HttpListenerResponse response)
         {
-            switch (url)
+            response.StatusCode = (int)HttpStatusCode.OK;
+            string responseString = "Helow, World!";
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            using (Stream stream = response.OutputStream)
             {
-                case "signal/":
-                    response.StatusCode = (int)HttpStatusCode.OK;
-                    string responseString = "Helow, World!";
-                    var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                    response.ContentLength64 = buffer.Length;
-                    using (Stream stream = response.OutputStream) 
-                    {
-                        stream.Write(buffer, 0, buffer.Length);
- 
-                    }
-                    break;
+                stream.Write(buffer, 0, buffer.Length);
+
+            }
+        }
+
+        private void Post(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            string str;
+            using (var reader = new StreamReader(request.InputStream,
+                                     request.ContentEncoding))
+            {
+                str = reader.ReadToEnd();
+            }
+            try
+            {
+                var signal = JsonConvert.DeserializeObject<Signal>(str);
+                List<Signal> signals = new List<Signal>();
+                signals.Add(signal);
+                var responseString = JsonConvert.SerializeObject(signals);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                using (Stream stream = response.OutputStream)
+                {
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch
+            {
+
             }
         }
     }
