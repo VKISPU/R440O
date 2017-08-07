7216dde92d85517bac2679c7a1b7a47a397a290b
Vitaliy Suhov <vitaliy-suhov@bk.com>
2017-08-07 12:41:30 +0400

    Added ui auto tests to power

---
+++ R440O.Test/BlockConfig/BaseBlockConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Extensions.Forms;
+
+namespace R440O.Test.BlockConfig
+{
+    abstract class BaseBlockConfig
+    {
+        public abstract string OpenFormButtonName
+        {
+            get;
+        }
+
+        public abstract string FormName
+        {
+            get;
+        }
+
+        protected bool формаОткрыта = false;
+        public virtual void ОткрытьФорму()
+        {
+            if (!формаОткрыта)
+            {
+                var buttonTester = new ButtonTester(OpenFormButtonName, BaseConstants.R440OForm);
+                buttonTester.FireEvent("Click");
+                формаОткрыта = true;
+            }
+        }
+
+        public virtual void ЗакрытьФорму()
+        {
+            if (формаОткрыта)
+            {
+                var formTester = new FormTester(FormName);
+                formTester.Close();
+                формаОткрыта = false;
+            }
+        }
+    }
+}
---
+++ R440O.Test/MainTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+
+using R440O.R440OForms.R440O;
+using R440O.R440OForms.C300M_1;
+using R440O.R440OForms.N502B;
+using R440O.Test.BlockConfig;
+
+namespace R440O.Test.MainTest
+{
+    [TestFixture]
+    class MainTest
+    {
+        //[Test]
+        //public void MainTest1()
+        //{
+        //    ParametersConfig.SetParameters();
+        //    var r440o = new R440OForm();
+        //    r440o.Show();
+        //    var buttonN15Tester = new ButtonTester("R440OButtonN15", "R440OForm");
+        //    buttonN15Tester.FireEvent("Click");
+        //    var button = new ButtonTester("КнопкаСтанцияВкл", "N15Form");
+        //    button.FireEvent("MouseDown", new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0));
+        //    button.FireEvent("MouseUp", new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0));
+        //    C300M_1Parameters.ЗначениеПоиска = 0;
+        //    Application.DoEvents();
+        //    Assert.IsTrue(C300M_1Parameters.СигналПойман);
+        //    var formTester = new FormTester("R440OForm");
+        //    formTester.Close();
+        //}
+
+        [Test]
+        public void MainTest2()
+        {
+            var r440oConfig = new R440OConfig();
+            var powerCabelConfig = new PowerCabelConfig();
+            var voltageStabilizerConfig = new VoltageStabilizerConfig();
+            var n502BConfig = new N502BConfig();
+
+            r440oConfig.ОткрытьФорму();
+
+            powerCabelConfig.ОткрытьФорму();
+            powerCabelConfig.ПодключитьКабель();
+            powerCabelConfig.ЗакрытьФорму();
+            
+            voltageStabilizerConfig.ОткрытьФорму();
+            voltageStabilizerConfig.ПодлючитьКабельВСоотвествииСНапряжением();
+            voltageStabilizerConfig.ЗакрытьФорму();
+                                  
+            n502BConfig.ОткрытьФорму();
+            n502BConfig.ВключитьВсеТумблеры();
+            n502BConfig.ВключитьСеть();
+            n502BConfig.Сфазировать();
+            n502BConfig.ЗакрытьФорму();
+            
+            Application.DoEvents();
+            Assert.IsTrue(N502BParameters.ЛампочкаСфазировано);
+
+            r440oConfig.ЗакрытьФорму();
+        }
+    }
+}
